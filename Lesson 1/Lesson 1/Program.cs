fa7acf4198bb6ae0c997e9cc426cc5c914eee2d8
Peter Wasonga Ombwa <pwombwa@live.com>
2017-11-18 12:29:02 +0000

    Added lesson 1

---
+++ Lesson 1/Lesson 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class WelcomeCSS
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the C# Station Tutorial!");
+
+            Console.ReadLine();
+        }
+    }
+}
